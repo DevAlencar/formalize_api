b6f67084eae6015764e225037241144fcc3b7bcd
Pedro Henrique Carvalho <113538363+pedrohcarvalh0@users.noreply.github.com>
2025-01-31 12:37:58 -0300

    Update settings.py
    Adição das seguintes linhas:
    # Caminho absoluto onde os arquivos estáticos serão coletados
    STATIC_ROOT = BASE_DIR / 'staticfiles'

--- formalize_api/settings.py
+++ formalize_api/settings.py
@@ -181,6 +181,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Caminho absoluto onde os arquivos estáticos serão coletados
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
